22ab057fce9710325a0fce4a9f2b32f08f08d26f
Stefanorn <stefanorn92@gmail.com>
2017-11-28 10:03:40 +0000

    Initial Commit

---
+++ PizzaStadur/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PizzaStadur
+//
+//  Created by Stefan Orn Hrafnsson on 28/11/2017.
+//  Copyright © 2017 Stefan Orn Hrafnsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

0b5512a4417dad80413a31d536f803ac229b5abf
Stefanorn <stefanorn92@gmail.com>
2017-11-28 12:28:01 +0000

    gerði pizzu

---
+++ PizzaStadur/Pizza.cpp
@@ -0,0 +1,9 @@
+//
+//  Pizza.cpp
+//  PizzaStadur
+//
+//  Created by Stefan Orn Hrafnsson on 28/11/2017.
+//  Copyright © 2017 Stefan Orn Hrafnsson. All rights reserved.
+//
+
+#include "Pizza.hpp"

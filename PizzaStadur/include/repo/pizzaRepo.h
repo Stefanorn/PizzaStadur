5ccb84290faec64face91ab0441f3f3b94a86f9a
stefanorn <stefanorn92@gmail.com>
2017-12-07 15:48:03 +0000

    Gerði pizza Repo

--- PizzaStadur/include/repo/pizzaRepo.h
+++ PizzaStadur/include/repo/pizzaRepo.h
@@ -1,16 +1,22 @@
 #ifndef PIZZAREPO_H
 #define PIZZAREPO_H
+#include "Pizza.h"
+#include <vector>
+#include <fstream>
 
+using namespace std;
 
 class pizzaRepo
 {
     public:
         pizzaRepo();
-        virtual ~pizzaRepo();
-
-    protected:
+        void RewriteFile( vector<Pizza> pizzas );
+        void WriteOrderToFile( Pizza pizzaToAdd );
 
     private:
+        void ReadFromFile();
+        vector<Pizza> _pizzas;
+
 };
 
 #endif // PIZZAREPO_H
--- PizzaStadur/src/repo/pizzaRepo.cpp
+++ PizzaStadur/src/repo/pizzaRepo.cpp
@@ -1,11 +1,55 @@
 #include "pizzaRepo.h"
 
-pizzaRepo::pizzaRepo()
-{
-    //ctor
+
+pizzaRepo::pizzaRepo(){
+    ReadFromFile();
 }
 
-pizzaRepo::~pizzaRepo()
-{
-    //dtor
+void pizzaRepo::ReadFromFile(){
+
+    ifstream stream;
+    stream.open( "pizza.bin", ios::binary );
+
+    if(stream.is_open() ){
+        stream.seekg(0,stream.end);
+        int length = stream.tellg() / sizeof(Pizza);
+        stream.seekg(0, stream.beg);
+
+        cout << "Reading " << length << " pizzas" << endl;
+
+        Pizza temp;
+        for(int i = 0; i < length; i++){
+            stream.read((char*)(&temp), sizeof(Pizza));
+            _pizzas.push_back(temp);
+        }
+    }
+    else{
+        cerr << "Could not find pizza.bin" << endl;
+    }
+
+    stream.close();
 }
+void pizzaRepo::RewriteFile( vector<Pizza> pizzas ){
+    ofstream stream;
+
+    stream.open("pizza.bin", ios::binary);
+    if(stream.is_open()){
+        stream.seekp( stream.beg );
+        for(unsigned int i = 0; i < pizzas.size(); i++){
+            stream.write((char*)(&pizzas[i]), sizeof(Pizza));
+        }
+    }
+
+    stream.close();
+    _pizzas = pizzas;
+}
+void pizzaRepo::WriteOrderToFile( Pizza pizzaToAdd ){
+
+    ofstream stream;
+    stream.open("pizza.bin", ios::binary|ios::app);
+    stream.write((char*)(&pizzaToAdd), sizeof(Pizza));
+    stream.close();
+
+    _pizzas.push_back(pizzaToAdd);
+}
+

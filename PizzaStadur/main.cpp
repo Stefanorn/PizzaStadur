a0d2080809b47ee1343e7baef789c19606b35a83
Tinna <tinnarut96@gmail.com>
2017-12-12 16:32:02 +0000

    Eyddi út klösum sem við erum ekki að nota og breytti exceptions í eina skrá

--- PizzaStadur/main.cpp
+++ PizzaStadur/main.cpp
@@ -1,4 +1,3 @@
-#include "Pizzasdadur.h"
 #include "StaffUI.h"
 #include "BakerUI.h"
 #include "SalesUI.h"
@@ -72,37 +71,4 @@ int main()
     }
 
     return 0;
-    Pizzasdadur pizzaPlace;
-    cout << "Welcome to n33rdy Pizza place" << endl;
-
-    while(true){
-        char userInput;
-        cout << "Press 1 to make order, \n2 to print all orders, \n3 to pay for order, \n4 to bake a pizza, \n5 to add item to database \nq to quit " << endl;
-        cin >> userInput;
-
-        switch (userInput){
-        case '1':
-            pizzaPlace.createOrder();
-            break;
-        case '2':
-            pizzaPlace.printAllOrders();
-            break;
-        case '3':
-            pizzaPlace.payForOrder();
-            break;
-        case '4':
-            pizzaPlace.bakePizza();
-            break;
-        case '5':
-            pizzaPlace.addItemToDataBase();
-            break;
-        case 'q':
-            return 0;
-            break;
-        default:
-            cout <<"Invalid input" << endl;
-            break;
-        }
-
-    }
 }
